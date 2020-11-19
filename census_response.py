a5a52431890e9801c4f03a535b10e28548b3b644
michaelpkuhn <rakuhn14@gmail.com>
2020-11-19 13:56:58 -0600

    created zip python file, json output file

--- census_response.py
+++ census_response.py
@@ -4,12 +4,14 @@ import requests
 
 def getCensusResponse(table_url,get_ls,geo):
     '''
-    Returns response from census query
+    Concatenates url string and returns response from census api query
     input:
-    
+        table_url (str): census api table url
+        get_ls (ls): list of tables to get data from
+        geo (str): geographic area and filter
     output:
+        response (requests.response): api response
     '''
-    
     get = 'NAME,' + ",".join(get_ls)
     url = f'{table_url}get={get}&for={geo}&key={census_key}'
     response = requests.get(url)
