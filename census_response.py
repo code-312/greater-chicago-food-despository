deacfb3fbad71df347c3adffe4503724851ceacf
michaelpkuhn <rakuhn14@gmail.com>
2020-12-03 14:40:11 -0600

    added table search function to census_response script

--- census_response.py
+++ census_response.py
@@ -16,3 +16,24 @@ def getCensusResponse(table_url,get_ls,geo):
     url = f'{table_url}get={get}&for={geo}&key={CENSUS_KEY}'
     response = requests.get(url)
     return(response)
+
+def searchTable(table_json_ls: list, keyword_ls: list, filter_function_ls=list()):
+    assert (type(table_json_ls)==type(keyword_ls)==type(filter_function_ls)==list), "searchTable Parameters must be lists"
+    return_json = list()
+    
+    for f in filter_function_ls:
+        table_json_ls = list(filter(f, table_json_ls))
+    #print(table_json_ls)
+    for d in table_json_ls:
+        try:
+            for k in keyword_ls:
+                if k.lower() in d[2].lower():
+                    continue
+                else:
+                    break
+            else:
+                return_json.append(d)
+        except: 
+            continue
+    
+    return return_json
