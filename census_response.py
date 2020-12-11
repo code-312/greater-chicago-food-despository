7ac7658c20c019f1315ed4c59da6afea69e26746
Christopher Cole <christophercole@Christophers-MacBook-Pro.local>
2020-12-10 20:30:33 -0600

    this is first commit

--- census_response.py
+++ census_response.py
@@ -2,10 +2,11 @@ from api_keys import census_key
 import json
 import requests
 
+
+
 def getCensusResponse(table_url,get_ls,geo):
     '''
-    Concatenates url string and returns response from census api query
-    input:
+
         table_url (str): census api table url
         get_ls (ls): list of tables to get data from
         geo (str): geographic area and filter
