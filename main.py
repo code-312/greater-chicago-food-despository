1942f7006a21a1a457e3aba9f17a56bee4ebbbf4
michaelpkuhn <mickuhn95@gmail.com>
2021-01-19 16:36:21 -0600

    added class approach for main.py

--- main.py
+++ main.py
@@ -1,42 +1,82 @@
 '''
 Defines and calls Census data requests
 '''
-#move this function to census_response.py
-from acs5racedemographics import processRaceData
 
-
-d = {
-    "race": {
-        "var_dict":{
-            'B03002_001E': 'race_total', 'B03002_005E': \
-            'race_native','B03002_004E': 'race_black', 'B03002_003E':\
-            'race_white', 'B03002_009E': 'race_twoplus_total', 'B03002_007E': 'race_pacific', \
-            'B03002_008E':'race_other', 'B03002_006E': 'race_asian',\
-            'B03002_012E': 'race_hispaniclatino_total'
+def dict_approach():
+    #move this function to census_response.py
+    from acs5racedemographics import processRaceData
+    d = {
+        "race": {
+            "var_dict":{
+                'B03002_001E': 'race_total', 'B03002_005E': \
+                'race_native','B03002_004E': 'race_black', 'B03002_003E':\
+                'race_white', 'B03002_009E': 'race_twoplus_total', 'B03002_007E': 'race_pacific', \
+                'B03002_008E':'race_other', 'B03002_006E': 'race_asian',\
+                'B03002_012E': 'race_hispaniclatino_total'
+            },
+            "table":'https://api.census.gov/data/2018/acs/acs5?',
+            "function_ls" : [processRaceData]
         },
-        "table":'https://api.census.gov/data/2018/acs/acs5?',
-        "function_ls" : [processRaceData]
-    },
-    "poverty":{
-        "var_dict":{
-            'S1701_C01_001E': 'poverty_population_total','S1701_C02_001E':'poverty_population_poverty'
-        },
-        "table":'https://api.census.gov/data/2018/acs/acs5/subject?',
-        "function_ls" : []
+        "poverty":{
+            "var_dict":{
+                'S1701_C01_001E': 'poverty_population_total','S1701_C02_001E':'poverty_population_poverty'
+            },
+            "table":'https://api.census.gov/data/2018/acs/acs5/subject?',
+            "function_ls" : []
+        }
     }
-}
+    return d
 
+
+def class_approach():
+    '''
+    Advantage to class approach is function ls, default parameter(s) set by default
+    Once implemented, attached function can update data for the class
+    '''
+    #move this function to census_response.py
+    from acs5racedemographics import processRaceData
+    class CensusData:
+        def __init__(self, var_dict, table, function_ls=[]):
+            self.var_dict = var_dict
+            self.table = table
+            self.function_ls = function_ls
+
+    #define race instance
+    race_dict = {'B03002_001E': 'race_total', 'B03002_005E': \
+                'race_native','B03002_004E': 'race_black', 'B03002_003E':\
+                'race_white', 'B03002_009E': 'race_twoplus_total', 'B03002_007E': 'race_pacific', \
+                'B03002_008E':'race_other', 'B03002_006E': 'race_asian',\
+                'B03002_012E': 'race_hispaniclatino_total'}
+    detailed_table = 'https://api.census.gov/data/2018/acs/acs5?'
+    race_functions = [processRaceData]
+    race = CensusData(race_dict, detailed_table, race_functions)
+
+    #define poverty instance
+    poverty_dict = {'S1701_C01_001E': 'poverty_population_total','S1701_C02_001E':'poverty_population_poverty'}
+    subject_table = 'https://api.census.gov/data/2018/acs/acs5/subject?'
+    poverty = CensusData(poverty_dict, subject_table)
+
+    #add instances to list
+    class_ls = [race, poverty]
+    return {i:c for i, c in enumerate(class_ls)}
 
 def main(d):
     import census_response
     d_ls = []
     for v in d.values():
-        var_dict, table, function_ls = v.values()
+        try:
+            var_dict, table, function_ls = v.values()
+        except:
+            var_dict = v.var_dict
+            table = v.table
+            function_ls = v.function_ls
         v_ls = census_response.getCensusData(var_dict, table, function_ls)
         d_ls += v_ls
     return d_ls
 
 if __name__ == '__main__':
+    d = dict_approach()
+    d = class_approach()
     main(d)
 
 '''
