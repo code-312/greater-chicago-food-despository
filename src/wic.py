7119e428679f953c8e9db672c14b7aa35fe91640
Zac Gross <brickman1444@gmail.com>
2021-03-28 11:57:34 -0500

    Clean Up wic.py
    -Change the column names to exactly match the column names from the pdf
    -Rephrase some comments to be clearer
    -Remove some redundant comments

--- src/wic.py
+++ src/wic.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 '''
 GCFD staff provided a pdf IWIC.pdf which contains WIC data for 96 out of 102 IL Counties. We do not know why some counties
-are missing. Data is for the month of January year 2021. WIC.py reads thru each line of each page of the pdf looking
+are missing. Data is for the month of January year 2021. wic.py reads thru each line of each page of the pdf looking
  for lines that contain the data we want and then saves that data to a dataframe. GCFD staff also requested a .csv version
  of the data'''
 
@@ -23,18 +23,16 @@ def parse_wic_pdf(source_pdf_filepath: str, destination_csv_filepath: str, first
     # find rows that start with three digits (these rows contain County ID and name, example: 031 COOK)
     County_re = re.compile(r"\d\d\d")
 
-    # define column names
-    # readable column names
-    column_names = ["County_ID", "County", "WIC1", "WIC2", "Amer. Indian or Alaska Native",
+    column_names = ["County_ID", "County", "WIC1", "WIC2", "Amer. Indian or Alaskan Native",
                     "Asian", "Black or African American", "Native Hawaii or Other Pacific Isl.",
-                    "White", "Multi Racial", "Total_Participants", "Hispanic_Latino"]
+                    "White", "Multi-Racial", "Total Participants", "Hispanic or Latino"]
 
     # create empty data frame with column names
     data = pd.DataFrame(columns=column_names)
 
     with pdfplumber.open(source_pdf_filepath) as pdf:
     
-        # range() excludes the last value, so add one so we read the last page
+        # range() excludes the last value, so add 1 to include the last page
         for page_num in range(first_page_zero_indexed, last_page_zero_indexed + 1):
             page = pdf.pages[page_num]
 
@@ -44,20 +42,16 @@ def parse_wic_pdf(source_pdf_filepath: str, destination_csv_filepath: str, first
 
             # iterate thru each line on a page
             for line in text.split("\n"):
-                # find the county info and save as variable County
-                # maxsplit=1 because some counties have spaces in their name
                 if County_re.match(line):
-                    County = (line.split(sep=" ", maxsplit=1))
-                # finds the lines that start with Total.
-                # appends to data
+                    # We have to find the County information first because we insert it in every row
+                    # maxsplit=1 because some counties have spaces in their name, example: Jo Daviess
+                    County = (line.split(sep=" ", maxsplit=1)) 
                 elif Total_re.match(line):
-                    new_line = (line.split(sep=" ")) # This looks something like "Total", "Women", 1, 2, 3, 4
+                    new_line = (line.split(sep=" ")) # This looks something like ["Total", "Women", 1, 2, 3, 4]
                     new_line = County + new_line
                     data = data.append(pd.Series(new_line, index=data.columns), ignore_index=True)
-                # finds lines that start with LA
-                # appends to data
                 elif County_Total_re.match(line):
-                    new_line = (line.split(sep=" "))
+                    new_line = (line.split(sep=" ")) # This looks something like ["LA", "Total", 1, 2, 3, 4]
                     new_line = County + new_line
                     data = data.append(pd.Series(new_line, index=data.columns), ignore_index=True)
 
