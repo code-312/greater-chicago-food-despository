9dbd4f7667792f0bc2f709bb7c8c91966e839504
Steve Woo <scwoo1@gmail.com>
2021-04-04 23:21:09 +0000

    I think I get it now... I can't have a method with the name `test_*`

--- tests/test_file_to_json.py
+++ tests/test_file_to_json.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(os.path.abspath(''))
 from src.file_to_json import file_to_json
 
-def test_output(actual_path, expected_path):
+def check_output(actual_path, expected_path):
     with open(actual_path) as actual_file:
         with open(expected_path) as expected_file:
             assert actual_file.read() == expected_file.read()
@@ -16,9 +16,9 @@ def clean_output(filepath):
 def test_file_to_json():
     try:
         file_to_json('tests/resources', 'tests/resources', blacklist=['Key'])
-        test_output('tests/resources/Countytest_food_insecurity_xls.json','tests/resources/test_food_insecurity_expected.json')
-        test_output('tests/resources/Countytest_food_insecurity_xlsx.json','tests/resources/test_food_insecurity_expected.json')
-        test_output('tests/resources/test_food_insecurity_csv.json','tests/resources/test_food_insecurity_expected.json')
+        check_output('tests/resources/Countytest_food_insecurity_xls.json','tests/resources/test_food_insecurity_expected.json')
+        check_output('tests/resources/Countytest_food_insecurity_xlsx.json','tests/resources/test_food_insecurity_expected.json')
+        check_output('tests/resources/test_food_insecurity_csv.json','tests/resources/test_food_insecurity_expected.json')
     finally:
         clean_output('tests/resources/Countytest_food_insecurity_xls.json')
         clean_output('tests/resources/Countytest_food_insecurity_xlsx.json')
