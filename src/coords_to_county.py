4136f14eb7a78bf9800f4da6fce57f4bfca4b48f
Steve Woo <scwoo1@gmail.com>
2021-04-23 02:57:44 +0000

    Fix flake8 errors

--- src/coords_to_county.py
+++ src/coords_to_county.py
@@ -2,7 +2,8 @@ import requests
 from typing import Any
 import json
 
-def query_fcc_api(lat:float, long: float) -> Any:
+
+def query_fcc_api(lat: float, long: float) -> Any:
     path = 'https://geo.fcc.gov/api/census/block/find'
     args = '?latitude={}&longitude={}&format=json'.format(lat, long)
     result = requests.get('{}{}'.format(path, args))
@@ -11,10 +12,12 @@ def query_fcc_api(lat:float, long: float) -> Any:
         raise Exception(err_response['statusMessage'])
     return json.loads(result.text)
 
+
 def coords_to_county(lat: float, long: float) -> tuple:
     raw_result = query_fcc_api(lat, long)
     county = raw_result['County']
-    return (county['FIPS'],county['name'])
+    return (county['FIPS'], county['name'])
+
 
 if __name__ == "__main__":
-    print(coords_to_county(40.882501,-87.647331))
+    print(coords_to_county(40.882501, -87.647331))
