7da9503fad3e13b06433c7cb9e90aa0f920aaaff
Steve Woo <scwoo1@gmail.com>
2021-05-10 03:13:07 +0000

    Got output working

--- src/snap.py
+++ src/snap.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import pandas as pd
 from typing import Any, Dict
 sys.path.append(os.path.abspath(''))
@@ -13,8 +14,9 @@ def load_excel(src: str) -> Any:
         table[k].dropna(axis=1, how='all', inplace=True)
         # drop empty columns (i.e. any value in column is NaN)
         table[k].dropna(axis=0, how='any', inplace=True)
+
         # drop last row (contains column sums)
-        table[k].drop(table[k].tail(1).index, inplace=True)
+        table[k] = table[k][table[k]['Unnamed: 0'] != 'Sum:']
 
         # ensure we're indexing from 0
         table[k].reset_index(inplace=True)
@@ -24,45 +26,76 @@ def load_excel(src: str) -> Any:
     return table
 
 
-def rename_columns(table: Dict[str, Any]) -> Any:
+def rename_columns(table: Dict[str, Any]) -> None:
     for k in table:
-        age_group = 'age_' + k.lower()[4:]
         columns = []
         columns.append('county')
-        columns.append('race_white_{}'.format(age_group))
-        columns.append('race_black_{}'.format(age_group))
-        columns.append('race_native_{}'.format(age_group))
-        columns.append('race_asian_{}'.format(age_group))
-        columns.append('race_pacific_{}'.format(age_group))
-        columns.append('race_hispaniclatino_{}'.format(age_group))
-        columns.append('race_unknown_{}'.format(age_group))
+        columns.append('race_white')
+        columns.append('race_black')
+        columns.append('race_native')
+        columns.append('race_asian')
+        columns.append('race_pacific')
+        columns.append('race_hispaniclatino')
+        columns.append('race_unknown')
         table[k].columns = columns
-    pass
 
 
-def add_fips_column(table: Dict[str, Any]) -> Any:
+def add_fips_column(table: Dict[str, Any]) -> None:
     fips = county_fips()
+    # We're going to make a separate dictionary where the keys are just
+    # the county name in all caps. Note that we're trimming out spaces
+    # as well to account for e.g. DE WITT vs. DEWITT
     abbr_fips = dict()
     for k in fips:
         abbr_key = k.upper().replace(' COUNTY, ILLINOIS', '').replace(' ', '')
         abbr_fips[abbr_key] = fips[k]
+
+    # Now that we have the new dictionary, populate the fips column
     for k in table:
         table[k]['fips'] = None
         for index, row in table[k].iterrows():
             fip = abbr_fips[row[0].replace(' ', '')]
             table[k].iloc[index, -1] = fip
+
+        # Set index to fips column
         table[k].set_index('fips', inplace=True)
-    pass
+
+        # drop column of county names
+        table[k].drop('county', axis=1, inplace=True)
+
+        print(table[k])
 
 
-def merge_snap_data(srcs: list) -> None:
+def to_dict(table: Dict[str, Any]) -> Dict[str, Dict[str, Dict[str, Any]]]:
+    output = dict()
+    for k in table:
+        age_group = 'age_' + k.lower()[4:]
+        as_dict = table[k].to_dict(orient='index')
+        output[age_group] = as_dict
+    return output
+
+
+# srcs is a list of tuples where the first element is the year,
+# second element is the path to the data source
+def merge_snap_data(srcs: list[tuple[str, str]], merge_to: Dict[str, Any]) -> Dict[str, Any]:  # noqa E501
     for src in srcs:
-        table = load_excel(src)
+        table = load_excel(src[1])
         rename_columns(table)
         add_fips_column(table)
-        print(table)
-    pass
+        table_dict = to_dict(table)
+        for fips in merge_to['county_data']:
+            for age_group in table_dict:
+                if 'snap_data' not in merge_to['county_data'][fips]:
+                    merge_to['county_data'][fips]['snap_data'] = dict()
+                if src[0] not in merge_to['county_data'][fips]['snap_data']:
+                    merge_to['county_data'][fips]['snap_data'][src[0]] = dict()
+                merge_to['county_data'][fips]['snap_data'][src[0]][age_group] = table_dict[age_group][fips]  # noqa E501
+    return merge_to
 
 
 if __name__ == '__main__':
-    merge_snap_data(['data_folder/SNAP_2019.xlsx'])
+    with open('final_jsons/df_merged_with_insecurity.json') as merged:
+        merged_data = json.load(merged)
+    with_snap = merge_snap_data([('2019', 'data_folder/SNAP_2019.xlsx'),('2020', 'data_folder/SNAP_2020.xlsx')], merged_data)  # noqa E501
+    with open('final_jsons/df_merged_with_insecurity_and_snap.json','w+') as new_merged:  # noqa E501
+        json.dump(with_snap, new_merged)
