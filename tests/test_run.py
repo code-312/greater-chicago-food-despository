66c242d4b35e42564163092e46fb1dfbcf2e5689
Zac Gross <brickman1444@gmail.com>
2021-05-25 21:51:41 -0500

    Add asserts that main outputs what we expect

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,13 @@
 import src.main
+import os
 
+def delete_file(path: str) -> None:
+    if os.path.exists(path):
+        os.remove(path)
 
 def test_run():
+    delete_file('final_jsons/df_dump.json')
+    delete_file('final_jsons/df_merged_json.json')
     src.main.main()
+    assert os.path.exists('final_jsons/df_dump.json')
+    assert os.path.exists('final_jsons/df_merged_json.json')
