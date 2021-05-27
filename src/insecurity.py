1f43b03949a5b3710bd88ed79d36d4041a3a5818
Zac Gross <brickman1444@gmail.com>
2021-05-27 17:46:49 -0500

    Add Bins for Food Insecurity Data (#61)
    * Add failing test for bins
    
    * Add enough data to the insecurity test data that we can bin it. Remove the separate file that I had intended to bin on it's own because everything has to go through the same function that will try to calculate bins and fail if there isn't enough data
    
    * Add quantiles bins
    
    * Add tests to verify the bins are present for all the insecurity fields because it isn't that hard to add
    
    * Fix flake8 errors
    
    * Nest the insecurity bins under a 'insecurity_data' object so the bins match the metrics per county
    
    * Update the expected output of the file_to_json test. I had forgotten that this uses the same input as the food insecurity test

--- src/insecurity.py
+++ src/insecurity.py
@@ -3,6 +3,11 @@ from src import data
 from src.file_to_json import files_to_dataframes  # noqa: E402
 
 
+column_names = ['insecurity_2018',
+                'insecurity_2020_projected',
+                'insecurity_2018_child',
+                'insecurity_2020_child_projected']
+
 def get_food_insecurity_data(input_dir: str = 'data_folder/insecurity') -> data.Wrapper:  # noqa E501
     tables = files_to_dataframes(input_dir, blacklist=['Key'])
 
@@ -10,6 +15,12 @@ def get_food_insecurity_data(input_dir: str = 'data_folder/insecurity') -> data.
     for pair in tables:
         df = reformat_dataframe(pair[1])
         combined_data.add(data.from_county_dataframe(df))
+        combined_data.meta.data_bins['natural_breaks'] = {
+            'insecurity_data': data.calculate_natural_breaks_bins(df, column_names)  # noqa E501
+        }
+        combined_data.meta.data_bins['quantiles'] = {
+            'insecurity_data': data.calculate_quantiles_bins(df, column_names)  # noqa E501
+        }
     return combined_data
 
 
@@ -19,8 +30,5 @@ def reformat_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     new_df = df.drop(labels='County Name', axis=1)
 
     # Rename columns to stay consistent with our column names elsewhere
-    new_df.columns = ['insecurity_2018',
-                      'insecurity_2020_projected',
-                      'insecurity_2018_child',
-                      'insecurity_2020_child_projected']
+    new_df.columns = column_names
     return new_df
